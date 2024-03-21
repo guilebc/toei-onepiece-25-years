bba1049baa4fbd3c1fb492f7d29ee3c9f253c94f
Guillaume <gui@nibelheimdesign.com>
2024-03-21 16:34:14 +0100

    Add instagram save posts command

---
+++ app/Console/Commands/InstagramContent.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\PostCity;
+use App\Enums\PostType;
+use App\Jobs\ProcessPost;
+use App\Models\Post;
+use App\Services\Azure\ContentSafetyInterface;
+use App\Services\Flockler\ContentInterface;
+use App\Services\Smartproxy\SocialMedia\Countries;
+use App\Services\Smartproxy\SocialMedia\InstagramInterface;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test command.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(ContentSafetyInterface $contentSafety, ContentInterface $content)
+    {
+        $last = Post::last()->first();
+
+        $content->posts()
+            ->where('flocklerId', '>', optional($last)->flockler_id ?? 0)
+            ->each(function($post) {
+
+                $tags = collect($post['tags']);
+                $images = collect($post['images']);
+
+                Post::create([
+                    'type' => PostType::INSTAGRAM,
+                    'city' => $tags->first(fn($tag) => PostCity::tryFrom($tag)) ?? PostCity::ALL,
+                    'flockler_id' => $post['flocklerId'],
+                    'image_url' => $images->first()['url'],
+                    'published_at' => $post['publishedAt'],
+                ]);
+
+                $this->info($post['flocklerId']);
+            });
+    }
+}
