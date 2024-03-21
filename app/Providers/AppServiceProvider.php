2e63c092e9f600369d572cbfd10e29b36e279782
Guillaume <gui@nibelheimdesign.com>
2024-03-21 16:36:40 +0100

    Factorize bind services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Services\Azure\ContentSafety;
+use App\Services\Azure\ContentSafetyInterface;
+use App\Services\Flockler\Content;
+use App\Services\Flockler\ContentInterface;
+use App\Services\Smartproxy\SocialMedia\Instagram;
+use App\Services\Smartproxy\SocialMedia\InstagramInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ContentSafetyInterface::class, ContentSafety::class);
+        $this->app->bind(InstagramInterface::class, Instagram::class);
+        $this->app->bind(ContentInterface::class, Content::class);
     }
 
     /**
