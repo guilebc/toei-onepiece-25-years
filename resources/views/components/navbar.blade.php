e3a1c5be67034707639f789f02c8f4ddf542b320
Guillaume <gui@nibelheimdesign.com>
2024-03-29 15:40:23 +0100

    Add navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="max-w-7xl mx-auto">
+    <div class="flex justify-between items-center">
+        <div class="w-2/12"><img class="w-[165px]" src="https://placehold.co/165x180.png" alt=""></div>
+        <div class="w-8/12">
+            <a href="{{ route('index') }}"><img class="mx-auto" src="https://placehold.co/355x125.jpg" alt=""></a>
+        </div>
+        <div class="w-2/12">
+            <div class="text-right">
+                <x-langue-selector/>
+            </div>
+        </div>
+    </div>
+</div>
