0e333d76e0dd23c1a4fafeff4ca07c69caf36c74
Guillaume <gui@nibelheimdesign.com>
2024-03-29 15:38:55 +0100

    Adjust langue selector component
    Re-style classes

--- resources/views/components/langue-selector.blade.php
+++ resources/views/components/langue-selector.blade.php
@@ -1,8 +1,8 @@
-<div x-data="{show: false}" class="relative">
-    <button @click="show = !show" @click.outside="show = false" type="button" class="flex items-center uppercase">{{ $currentLocale }}</button>
-    <ul x-cloak x-transition.opacity x-show="show" class="text-lg w-32 absolute right-0 bg-gray border border-green text-right p-3">
+<div x-data="{show: false}" class="relative inline-block">
+    <button @click="show = !show" @click.outside="show = false" type="button" class="text-white flex items-center px-3 uppercase rounded-full border border-white">{{ $currentLocale }}</button>
+    <ul x-cloak x-transition.opacity x-show="show" class="text-lg text-right w-32 absolute right-0 bg-white rounded p-3">
         @foreach($supportedLocales as $key => $locale)
-            <li><a class="transition-colors hover:text-green" href="{{ LaravelLocalization::getLocalizedUrl($key) }}">{{ $locale['native'] }}</a></li>
+            <li><a class="transition-colors hover:text-primary" href="{{ LaravelLocalization::getLocalizedUrl($key) }}">{{ $locale['native'] }}</a></li>
         @endforeach
     </ul>
 </div>
