8fff921348c1b82dc46e4eadf4ae6cd0dc1dc983
Guillaume <gui@nibelheimdesign.com>
2024-03-29 15:43:48 +0100

    Add app script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-// import './bootstrap';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import {Fancybox} from "@fancyapps/ui";
+import countdown from "./countdown.js";
+import carousel from "./carousel.js";
+
+Alpine.data('countdown', countdown);
+Alpine.data('carousel', carousel);
+Fancybox.bind('[data-fancybox]');
+
+Livewire.start();
+
+// https://placehold.co/120x160.png
