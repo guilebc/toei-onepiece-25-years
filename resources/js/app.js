f8b86126b094ec2eef0da1d97fc0babec25f8499
Guillaume <gui@nibelheimdesign.com>
2024-04-02 22:18:04 +0200

    Add dayjs package

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,14 @@ import {Fancybox} from "@fancyapps/ui";
 import countdown from "./countdown.js";
 import carousel from "./carousel.js";
 import captcha from "./captcha.js";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.setDefault('Europe/Paris');
 
 Alpine.data('countdown', countdown);
 Alpine.data('carousel', carousel);
