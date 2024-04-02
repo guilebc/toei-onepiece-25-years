965536a2cb97f17957aefeb7334fd29b99a1e99d
Guillaume <gui@nibelheimdesign.com>
2024-04-02 22:20:11 +0200

    Convert partner section component

---
+++ resources/views/components/partner-section.blade.php
@@ -0,0 +1,11 @@
+<section class="partner-section">
+    <div class="max-w-7xl mx-auto py-12">
+        <ul class="flex flex-wrap lg:flex-nowrap justify-center gap-3 text-center">
+            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Plastoy"></a></li>
+            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Ultra Pop"></a></li>
+            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Bandai Namco"></a></li>
+            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Funko"></a></li>
+            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Crunchyroll"></a></li>
+        </ul>
+    </div>
+</section>
