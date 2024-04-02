9f9cc2155a1b350738f952e3199cdd09d413e487
Guillaume <gui@nibelheimdesign.com>
2024-04-02 22:19:24 +0200

    Convert explain section component

---
+++ resources/views/components/explain-section.blade.php
@@ -0,0 +1,19 @@
+<section class="bg-neutral-200">
+    <div class="max-w-7xl mx-auto">
+        <div class="lg:flex lg:justify-center lg:items-center lg:gap-32">
+            <div class="lg:w-4/12">
+                <div><img class="mx-auto" src="https://placehold.co/490x695.jpg" alt=""></div>
+            </div>
+            <div class="lg:w-5/12">
+                <div class="p-6">
+                    <h1 class="font-display text-4xl uppercase mb-6">{{ trans('app.happyBirthdayLuffy') }}</h1>
+                    <p class="font-bold mb-6">{{ trans('app.luffyCelebrationText1') }}</p>
+                    <p class="mb-6">{{ trans('app.luffyCelebrationText2') }}</p>
+                    <div class="text-center lg:text-left">
+                        <a href="#" class="btn-primary px-8 py-3 transition-colors hover:bg-black hover:text-white">{{ trans('app.officialWebsite') }}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
