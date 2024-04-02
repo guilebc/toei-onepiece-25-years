bc6b3f54b8a66f32197ec63e7d20fb63e7b7e2a7
Guillaume <gui@nibelheimdesign.com>
2024-04-02 22:20:29 +0200

    Apply all converted components

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,83 +6,15 @@
         <x-navbar theme="black"/>
     </section>
 
-    <div class="hero-section">
-        <div class="relative">
-            <div class="absolute top-0 z-10 w-full">
-                <x-navbar/>
-            </div>
-            <div class="relative">
-                <div><img src="https://placehold.co/1920x860.jpg" alt=""></div>
-                <div class="w-full absolute bottom-12 text-center">
-                    <a data-fancybox href="https://www.youtube.com/watch?v=PwVT67T5Xt4" class="btn-primary px-8 py-3 transition-colors hover:bg-black hover:text-white">{{ trans('app.watchTrailer') }}</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-hero-section/>
 
     <x-posts-section/>
     <x-bestof-section/>
     <x-stream-section/>
     <x-countdown-section/>
-
-    <section class="bg-neutral-200">
-        <div class="max-w-7xl mx-auto">
-            <div class="lg:flex lg:justify-center lg:items-center lg:gap-32">
-                <div class="lg:w-4/12">
-                    <div><img class="mx-auto" src="https://placehold.co/490x695.jpg" alt=""></div>
-                </div>
-                <div class="lg:w-5/12">
-                    <div class="p-6">
-                        <h1 class="font-display text-4xl uppercase mb-6">{{ trans('app.happyBirthdayLuffy') }}</h1>
-                        <p class="font-bold mb-6">{{ trans('app.luffyCelebrationText1') }}</p>
-                        <p class="mb-6">{{ trans('app.luffyCelebrationText2') }}</p>
-                        <div class="text-center lg:text-left">
-                            <a href="#" class="btn-primary px-8 py-3 transition-colors hover:bg-black hover:text-white">{{ trans('app.officialWebsite') }}</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <section class="partner-section">
-        <div class="max-w-7xl mx-auto py-12">
-            <ul class="flex flex-wrap lg:flex-nowrap justify-center gap-3 text-center">
-                <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Plastoy"></a></li>
-                <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Ultra Pop"></a></li>
-                <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Bandai Namco"></a></li>
-                <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Funko"></a></li>
-                <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Crunchyroll"></a></li>
-            </ul>
-        </div>
-    </section>
-
-
-    <footer class="bg-black">
-        <div class="max-w-7xl mx-auto py-12">
-            <div class="text-white uppercase lg:flex lg:items-center lg:gap-12">
-                <div class="mb-6"><a href="#"><img class="mx-auto" src="https://placehold.co/260x155.png" alt="Toei Animation"></a></div>
-
-                <div>
-                    <div class="flex justify-center lg:justify-start items-center gap-3 mb-6">
-                        <div><a href="#"><img src="https://placehold.co/30x30.png" alt="Instagram"></a></div>
-                        <div><a href="#"><img src="https://placehold.co/30x30.png" alt="X"></a></div>
-                        <div><a  class="hover:text-primary" href="https://www.toei-animation.com/" target="_blank">toei-animation.com</a></div>
-                    </div>
-
-                    <div class="flex flex-wrap justify-center lg:justify-start gap-3 mb-6">
-                        @foreach(LaravelLocalization::getSupportedLocales() as $key => $locale)
-                        <a class="hover:text-primary" href="{{ LaravelLocalization::getLocalizedUrl($key) }}">{{ $locale['native'] }}</a>
-                        @endforeach
-                    </div>
-
-                    <div class="text-sm text-center lg:text-left"><a  class="hover:text-primary" href="#">{{ trans('app.legalNotice') }}</a></div>
-                    <p class="text-sm text-center lg:text-left">&copy; Eiichiro Oda/Shueisha, Toei Animation</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <x-explain-section/>
+    <x-partner-section/>
+    <x-footer/>
 
     <x-modal>
         <livewire:create-post/>
