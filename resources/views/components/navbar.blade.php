43a320b351c0c382b1bf76906635d98e6b14082f
Guillaume <gui@nibelheimdesign.com>
2024-04-05 14:46:41 +0200

    Add logo asset

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 @props(['theme'])
-<div class="max-w-7xl mx-auto">
+<div {{ $attributes->merge(['class' => 'max-w-7xl mx-auto']) }}>
     <div class="flex justify-center sm:justify-between items-center ">
-        <div class="w-3/12 sm:w-2/12"><img class="w-[165px]" src="https://placehold.co/165x180.png" alt=""></div>
+        <div class="w-3/12 sm:w-2/12"><img class="w-[165px]" src="{{ asset('assets/logo.png') }}" alt=""></div>
         <div class="w-5/12 sm:w-8/12">
             <a href="{{ route('index') }}"><img class="mx-auto" src="https://placehold.co/355x125.jpg" alt=""></a>
         </div>
