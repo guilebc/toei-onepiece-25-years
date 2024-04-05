fe11b610bbffa966692cdd0e129ab4ff6b1c0dc1
Guillaume <gui@nibelheimdesign.com>
2024-04-05 14:45:05 +0200

    Add partners logo

--- resources/views/components/partner-section.blade.php
+++ resources/views/components/partner-section.blade.php
@@ -1,11 +1,13 @@
 <section class="partner-section">
     <div class="max-w-7xl mx-auto py-12">
         <ul class="flex flex-wrap lg:flex-nowrap justify-center gap-3 text-center">
-            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Plastoy"></a></li>
-            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Ultra Pop"></a></li>
-            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Bandai Namco"></a></li>
-            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Funko"></a></li>
-            <li><a href="#"><img src="https://placehold.co/260x155.png" alt="Crunchyroll"></a></li>
+            <li><a href="#"><img src="{{ asset('assets/partners/abystyle.jpg') }}" alt="Abystyle"></a></li>
+            <li><a href="#"><img src="{{ asset('assets/partners/bandai.jpg') }}" alt="Bandai Namco"></a></li>
+            <li><a href="#"><img src="{{ asset('assets/partners/crunchyroll.jpg') }}" alt="Crunchyroll"></a></li>
+            <li><a href="#"><img src="{{ asset('assets/partners/funko.jpg') }}" alt="Funko"></a></li>
+            <li><a href="#"><img src="{{ asset('assets/partners/lucca.jpg') }}" alt="Lucca"></a></li>
+            <li><a href="#"><img src="{{ asset('assets/partners/plastoy.jpg') }}" alt="Plastoy"></a></li>
+            <li><a href="#"><img src="{{ asset('assets/partners/ultrapop.jpg') }}" alt="Ultrapop"></a></li>
         </ul>
     </div>
 </section>
