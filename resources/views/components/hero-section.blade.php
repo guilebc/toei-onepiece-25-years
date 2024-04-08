ddb995e92dd61f01c0b9698359c9d565b4c46cd9
Guillaume <gui@nibelheimdesign.com>
2024-04-08 16:06:27 +0200

    Fix stream 25th logo

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -2,7 +2,7 @@
     <div class="lg:flex lg:items-center text-white bg-prussian-blue">
         <div class="lg:w-1/2 flex justify-center items-center pt-6 mb-6 lg:mb-0">
             <div class="text-center">
-                <div><img class="mx-auto mb-3" src="{{ asset('assets/logo.png') }}" alt="TV Animation One Piece 25th"></div>
+                <div><img class="max-w-[200px] lg:max-w-full mx-auto mb-3" src="{{ asset('assets/logo.png') }}" alt="TV Animation One Piece 25th"></div>
                 <div class="font-display uppercase text-5xl px-6 mb-3">{{ trans('app.happyBirthdayLuffy') }}</div>
                 <div class="font-bold text-2xl px-6">{{ trans('app.stayConnected') }}</div>
             </div>
