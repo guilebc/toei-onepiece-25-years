1eeaebd7ef82f43c031187d69d6aa1abfde9c531
Guillaume <gui@nibelheimdesign.com>
2024-04-08 14:10:49 +0200

    Add playing button

--- resources/views/components/bestof-section.blade.php
+++ resources/views/components/bestof-section.blade.php
@@ -9,8 +9,11 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-12">
                 @foreach($videos as $video)
                     <div>
-                        <div class="after:block after:w-1/4 after:h-2 after:bg-{{ $video['color'] }}">
+                        <div class="relative after:block after:w-1/4 after:h-2 after:bg-{{ $video['color'] }}">
                             <a data-fancybox href="{{ $video['video_url'] }}"><img src="{{ $video['thumbnail_url'] }}" alt="{{ $video['title'] }}"></a>
+                            <div class="flex justify-center items-center absolute inset-0 z-10 pointer-events-none">
+                                <button type="button" class="w-16 h-16 transition-opacity"><img src="{{ asset('assets/play.png') }}" alt=""></button>
+                            </div>
                         </div>
                         <div>
                             <span class="font-bold text-lg">{{ $video['title'] }}</span><br />
--- resources/views/components/trailer-section.blade.php
+++ resources/views/components/trailer-section.blade.php
@@ -5,10 +5,15 @@
             <p class="font-display text-2xl uppercase">{{ trans('app.firstGift') }}</p>
         </div>
 
-        <div class="px-3">
-            <video class="block aspect-video w-full max-w-3xl mx-auto border border-white" controls >
-                <source src="https://player.vimeo.com/progressive_redirect/playback/924569081/rendition/720p/file.mp4?loc=external&log_user=0&signature=f9a9d385f07d159aae4ddc9330073a6dfd99dfad9578519e169151de51c09f17" type="video/mp4" />
-            </video>
+        <div class="px-3" x-data="player">
+            <div class="relative">
+                <video x-ref="video" @play="onPlay" @ended="onEnded" @pause="onPause" class="block aspect-video w-full max-w-3xl mx-auto border border-white" controls >
+                    <source src="https://player.vimeo.com/progressive_redirect/playback/924569081/rendition/720p/file.mp4?loc=external&log_user=0&signature=f9a9d385f07d159aae4ddc9330073a6dfd99dfad9578519e169151de51c09f17" type="video/mp4" />
+                </video>
+                <div class="flex justify-center items-center absolute inset-0 z-10">
+                    <button x-ref="playButton" type="button" class="w-24 h-24 transition-opacity" @click="toggle"><img src="{{ asset('assets/play.png') }}" alt=""></button>
+                </div>
+            </div>
         </div>
     </div>
 </section>
