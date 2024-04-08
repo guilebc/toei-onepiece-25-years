d93b109d4975c203e23bcea041a0c81d9768f46f
Guillaume <gui@nibelheimdesign.com>
2024-04-08 14:49:57 +0200

    Add partners links

--- resources/views/components/partner-section.blade.php
+++ resources/views/components/partner-section.blade.php
@@ -1,13 +1,13 @@
 <section class="partner-section">
     <div class="max-w-7xl mx-auto py-12">
         <ul class="flex flex-wrap lg:flex-nowrap justify-center gap-3 text-center">
-            <li><a href="#"><img src="{{ asset('assets/partners/ultrapop.jpg') }}" alt="Ultrapop"></a></li>
-            <li><a href="#"><img src="{{ asset('assets/partners/crunchyroll.jpg') }}" alt="Crunchyroll"></a></li>
-            <li><a href="#"><img src="{{ asset('assets/partners/abystyle.jpg') }}" alt="Abystyle"></a></li>
-            <li><a href="#"><img src="{{ asset('assets/partners/bandai.jpg') }}" alt="Bandai Namco"></a></li>
-            <li><a href="#"><img src="{{ asset('assets/partners/funko.jpg') }}" alt="Funko"></a></li>
-            <li><a href="#"><img src="{{ asset('assets/partners/lucca.jpg') }}" alt="Lucca"></a></li>
-            <li><a href="#"><img src="{{ asset('assets/partners/plastoy.jpg') }}" alt="Plastoy"></a></li>
+            <li><a target="_blank" href="https://ultrapop.co/"><img src="{{ asset('assets/partners/ultrapop.jpg') }}" alt="Ultrapop"></a></li>
+            <li><a target="_blank" href="https://www.crunchyroll.com/"><img src="{{ asset('assets/partners/crunchyroll.jpg') }}" alt="Crunchyroll"></a></li>
+            <li><a target="_blank" href="https://www.abystyle-studio.com/"><img src="{{ asset('assets/partners/abystyle.jpg') }}" alt="ABYstyle"></a></li>
+            <li><a target="_blank" href="https://fr.bandainamcoent.eu/"><img src="{{ asset('assets/partners/bandai.jpg') }}" alt="Bandai Namco"></a></li>
+            <li><a target="_blank" href="https://funkoeurope.com/"><img src="{{ asset('assets/partners/funko.jpg') }}" alt="Funko"></a></li>
+            <li><a target="_blank" href="https://www.luccacomicsandgames.com/en/2023/home/"><img src="{{ asset('assets/partners/lucca.jpg') }}" alt="Lucca"></a></li>
+            <li><a target="_blank" href="https://plastoy.fr/"><img src="{{ asset('assets/partners/plastoy.jpg') }}" alt="Plastoy"></a></li>
         </ul>
     </div>
 </section>
