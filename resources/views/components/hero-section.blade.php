5533d02535ec31bf8bcc87c64b91e471127fa43f
Guillaume <gui@nibelheimdesign.com>
2024-04-08 11:04:52 +0200

    Rebuild hero component

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -1,8 +1,14 @@
 <section class="hero-section">
-    <div class="relative">
-        <div><img src="https://placehold.co/1920x860.jpg" alt=""></div>
-        <div class="w-full absolute bottom-12 text-center">
-            <a data-fancybox href="{{ config('app.trailer_url') }}" class="btn-primary px-8 py-3 transition-colors hover:bg-black hover:text-white">{{ trans('app.watchTrailer') }}</a>
+    <div class="lg:flex lg:items-center">
+        <div class="lg:w-1/2 flex justify-center items-center mb-6 lg:mb-0">
+            <div class="text-center">
+                <div><img class="mx-auto mb-3" src="https://placehold.co/315x340.jpg" alt="TV Animation One Piece 25th"></div>
+                <div class="font-display uppercase text-5xl px-6 mb-3">{{ trans('app.happyBirthdayLuffy') }}</div>
+                <div class="font-bold text-2xl px-6">{{ trans('app.stayConnected') }}</div>
+            </div>
+        </div>
+        <div class="lg:w-1/2">
+            <div class="aspect-square"><img class="object-cover w-full h-full" src="https://placehold.co/995x860.jpg" alt="TV Animation One Piece 25th"></div>
         </div>
     </div>
 </section>
