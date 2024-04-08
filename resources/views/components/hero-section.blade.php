b2200b3bfaa00fbd6a5eb7a888058c6227f6c4c1
Guillaume <gui@nibelheimdesign.com>
2024-04-08 14:47:32 +0200

    Add asset to hero section

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -2,13 +2,14 @@
     <div class="lg:flex lg:items-center text-white bg-prussian-blue">
         <div class="lg:w-1/2 flex justify-center items-center pt-6 mb-6 lg:mb-0">
             <div class="text-center">
-                <div><img class="mx-auto mb-3" src="https://placehold.co/315x340.jpg" alt="TV Animation One Piece 25th"></div>
+                <div><img class="mx-auto mb-3" src="{{ asset('assets/logo.png') }}" alt="TV Animation One Piece 25th"></div>
                 <div class="font-display uppercase text-5xl px-6 mb-3">{{ trans('app.happyBirthdayLuffy') }}</div>
                 <div class="font-bold text-2xl px-6">{{ trans('app.stayConnected') }}</div>
             </div>
         </div>
         <div class="lg:w-1/2">
-            <div class="aspect-square"><img class="object-cover w-full h-full" src="https://placehold.co/995x860.jpg" alt="TV Animation One Piece 25th"></div>
+            <div class="aspect-square"><img class="object-cover w-full h-full" src="{{ asset('assets/hero.jpg') }}" alt="TV Animation One Piece 25th"></div>
         </div>
     </div>
 </section>
+
