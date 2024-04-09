d213a4a6bf039d43665be9cf7ca2d00b4fd7934b
Guillaume <gui@nibelheimdesign.com>
2024-04-09 10:58:18 +0200

    Fix modal close button

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -2,7 +2,7 @@
     <div x-transition.opacity class="fixed inset-0 bg-gray-500 bg-opacity-75"></div>
     <div x-show="open" x-transition.opacity class="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div class="min-h-full flex justify-center items-center p-3">
-            <div @click.outside="open = false" class="relative overflow-hidden rounded-lg bg-white text-left shadow-xl w-full max-w-5xl py-6">
+            <div @click.outside="open = false" class="relative overflow-hidden rounded-lg bg-white text-left shadow-xl w-full max-w-5xl py-9">
                 <button type="button" @click="open = false" class="absolute top-3 right-3 transition-colors hover:text-primary">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
