a0dbf455a3430acbfb32a9d3745017f4c6168160
Guillaume <gui@nibelheimdesign.com>
2024-04-10 17:12:33 +0200

    Reup hero baseline section

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -4,6 +4,7 @@
             <div class="text-center">
                 <div><img class="max-w-[200px] lg:max-w-full mx-auto mb-3" src="{{ asset('assets/logo.png') }}" alt="TV Animation One Piece 25th"></div>
                 <div class="font-display uppercase text-5xl px-6 mb-3">{{ trans('app.happyBirthdayLuffy') }}</div>
+                <div class="font-bold text-2xl px-6">{{ trans('app.stayConnected') }}</div>
             </div>
         </div>
         <div class="lg:w-1/2">
