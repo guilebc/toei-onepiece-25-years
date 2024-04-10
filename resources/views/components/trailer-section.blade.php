6e0be41b4434fbebf36f2677f99fe0f08bca1229
Guillaume <gui@nibelheimdesign.com>
2024-04-10 17:20:17 +0200

    Add trailer video

--- resources/views/components/trailer-section.blade.php
+++ resources/views/components/trailer-section.blade.php
@@ -6,8 +6,8 @@
 
         <div class="px-3" x-data="player">
             <div class="relative">
-                <video x-ref="video" @play="onPlay" @ended="onEnded" @pause="onPause" class="block aspect-video w-full max-w-3xl mx-auto border border-white" controls >
-                    <source src="https://player.vimeo.com/progressive_redirect/playback/924569081/rendition/720p/file.mp4?loc=external&log_user=0&signature=f9a9d385f07d159aae4ddc9330073a6dfd99dfad9578519e169151de51c09f17" type="video/mp4" />
+                <video x-ref="video" @play="onPlay" @ended="onEnded" @pause="onPause" class="block aspect-video w-full max-w-4xl mx-auto border border-white" controls >
+                    <source src="https://player.vimeo.com/progressive_redirect/playback/932926009/rendition/1080p/file.mp4?loc=external&log_user=0&signature=29f850bd6097372bf9a9b331d84f373c4cc02e8a67efa888ec368ea48cbf635a" type="video/mp4" />
                 </video>
                 <div class="flex justify-center items-center absolute inset-0 z-10 pointer-events-none">
                     <button x-ref="playButton" type="button" class="w-24 h-24 transition-opacity pointer-events-auto" @click="toggle"><img src="{{ asset('assets/play.png') }}" alt=""></button>
