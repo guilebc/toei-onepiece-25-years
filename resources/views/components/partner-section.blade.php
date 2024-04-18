a4c3b5f6a55f6c6379242ee8e74a2ca6a247b895
Guillaume <gui@nibelheimdesign.com>
2024-04-18 11:45:59 +0200

    Add bander partner

--- resources/views/components/partner-section.blade.php
+++ resources/views/components/partner-section.blade.php
@@ -1,9 +1,10 @@
 <section class="partner-section">
     <div class="max-w-7xl mx-auto px-3 lg:px-0 py-12">
-        <ul class="grid grid-cols-2 lg:flex lg:flex-nowrap justify-center gap-3 text-center">
+        <ul class="grid grid-cols-2 lg:flex lg:flex-wrap justify-center gap-6 text-center">
             <li><a target="_blank" href="https://ultraicetea.com/en/home/"><img src="{{ asset('assets/partners/ultrapop.jpg') }}" alt="Ultrapop"></a></li>
             <li><a target="_blank" href="https://www.crunchyroll.com/"><img src="{{ asset('assets/partners/crunchyroll.jpg') }}" alt="Crunchyroll"></a></li>
             <li><a target="_blank" href="https://www.abystyle-studio.com/"><img src="{{ asset('assets/partners/abystyle.jpg') }}" alt="ABYstyle"></a></li>
+            <li><a target="_blank" href="https://www.bandai.fr/"><img src="{{ asset('assets/partners/bandai.png') }}" alt="Bandai"></a></li>
             <li><a target="_blank" href="https://fr.bandainamcoent.eu/"><img src="{{ asset('assets/partners/bandai.jpg') }}" alt="Bandai Namco"></a></li>
             <li><a target="_blank" href="https://funkoeurope.com/"><img src="{{ asset('assets/partners/funko.jpg') }}" alt="Funko"></a></li>
             <li><a target="_blank" href="https://www.luccacomicsandgames.com/en/2023/home/"><img src="{{ asset('assets/partners/lucca.jpg') }}" alt="Lucca"></a></li>
