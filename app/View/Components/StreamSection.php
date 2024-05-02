d5e4c802bf0965652830f880b838d3f293159d31
Guillaume <gui@nibelheimdesign.com>
2024-05-02 11:18:32 +0200

    Add youtube ids

--- app/View/Components/StreamSection.php
+++ app/View/Components/StreamSection.php
@@ -27,31 +27,31 @@ class StreamSection extends Component
             'name' => 'Milan',
             'color' => 'milan',
             'stream_url' => asset('assets/milan.mp4'),
-            'youtube_id' => 'RTwNEfirmks',
+            'youtube_id' => 'iH5OWAiSQOQ',
         ], [
             'slug' => 'berlin',
             'name' => 'Berlin',
             'color' => 'berlin',
             'stream_url' => asset('assets/berlin.mp4'),
-            'youtube_id' => 'MWZpVfH58qc',
+            'youtube_id' => 'DN7f38uhvSw',
         ], [
             'slug' => 'london',
             'name' => trans('app.london'),
             'color' => 'london',
             'stream_url' => asset('assets/london.mp4'),
-            'youtube_id' => 'RTwNEfirmks',
+            'youtube_id' => 'jJHe47wL_lQ',
         ], [
             'slug' => 'madrid',
             'name' => 'Madrid',
             'color' => 'madrid',
             'stream_url' => asset('assets/madrid.mp4'),
-            'youtube_id' => 'MWZpVfH58qc',
+            'youtube_id' => 'f1jUP0mi5dg',
         ], [
             'slug' => 'paris',
             'name' => 'Paris',
             'color' => 'paris',
             'stream_url' => asset('assets/paris.mp4'),
-            'youtube_id' => 'RTwNEfirmks',
+            'youtube_id' => 'j0nPMprOMqQ',
         ]]);
 
         return view('components.stream-section', compact('streams'));
