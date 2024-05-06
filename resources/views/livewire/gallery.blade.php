d4335a206d2fbac3de075d130de56ca4301378b8
Guillaume <gui@nibelheimdesign.com>
2024-05-06 12:58:46 +0200

    Fix

--- resources/views/livewire/gallery.blade.php
+++ resources/views/livewire/gallery.blade.php
@@ -1,7 +1,8 @@
 <div>
     <div class="grid grid-cols-2 lg:grid-cols-4 gap-3 mb-6">
         @foreach($images as $index => $image)
-            <div class="aspect-square" wire:key="{{ $index }}" data-aos="fade" data-aos-delay="{{ mt_rand(1, 3) * 100 }}">
+            {{ $image }}
+            <div class="aspect-square" data-aos="fade" data-aos-delay="{{ mt_rand(1, 3) * 100 }}">
                 <img class="w-full h-full object-cover" src="{{ $image->url }}" alt="">
             </div>
         @endforeach
