8a048ef4bcf340eff052afef6b21cadf7d383c30
Guillaume <gui@nibelheimdesign.com>
2024-05-06 09:38:22 +0200

    End

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -3,8 +3,8 @@
         <div class="lg:w-1/2 flex justify-center items-center pt-6 mb-6 lg:mb-0">
             <div class="text-center">
                 <div><img class="max-w-[200px] lg:max-w-full mx-auto mb-3" src="{{ asset('assets/logo.png') }}" alt="TV Animation One Piece 25th"></div>
-                <div class="font-display uppercase text-5xl px-6 mb-3">{{ trans('app.happyBirthdayLuffy') }}</div>
-                <div class="font-bold text-2xl px-6">{{ trans('app.stayConnected') }}</div>
+                <div class="font-display uppercase text-5xl px-6 mb-3">{{ trans('app.galleryTitle') }}</div>
+                {{--<div class="font-bold text-2xl px-6">{{ trans('app.stayConnected') }}</div>--}}
             </div>
         </div>
         <div class="lg:w-1/2">
